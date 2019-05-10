2905e3ca1035654f25cdd76c27cc90a312013498
x0y <x0y@milkyweb.co>
2019-05-10 18:52:24 +0200

    [add] - setup added to the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# schemy
-A graphQl server in python
+# Schemy
+
+A tool to generate GraphQl APIs from a GraphQl Schema in python. It has its own server
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='schemy',
+      version='0.1',
+      description='A graphQl server',
+      long_description='A tool to auto-generate graqhQl types and models from a schema, with a server included to makeit a graphQl API Server',
+      keywords='graphql schema api python3',
+      url='https://github.com/x0y-gt/schemy',
+      author='Sebastian Godoy',
+      author_email='sebas@milkyweb.co',
+      license='GNUv3',
+      packages=find_packages(),
+      install_requires=[
+          'Click==7.0',
+          'GraphQL-core-next==1.0.3'
+      ],
+)
