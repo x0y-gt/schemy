e2f79af0a7fd959a8d6f334a7e2222a149e51a70
x0y <x0y@milkyweb.co>
2019-05-20 21:42:17 +0200

    [upd] Changes to map queries to add the arguments info

--- schemy/graphql/utils/map_schema_queries.py
+++ schemy/graphql/utils/map_schema_queries.py
@@ -1,25 +1,36 @@
+from graphql import get_named_type
 from schemy.graphql.utils.map_schema_types import map_schema_types
+
+from pprint import pprint
 
 __all__ = ["map_schema_resolvers"]
 
 def map_schema_queries(schema, base='Query'):
     """Returns a list that contains all the path queries from a base object like Query of Mutation
 
+    :param base: can be Query or Mutation
+
     This is used to build a list of resolvers for a graphQl schema
     Example of return:
     [
-    [{'field': 'publisher', 'list': False, 'nullable': True, 'type': 'Publisher'},
-     {'field': 'books', 'list': True, 'nullable': True, 'type': 'Book'},
-     {'field': 'authors', 'list': True, 'nullable': True, 'type': 'Author'}],
+    [{'field': 'publisher', 'list': False, 'nullable': True, 'type': 'Publisher', args: {}},
+     {'field': 'books', 'list': True, 'nullable': True, 'type': 'Book', args: {}},
+     {'field': 'authors', 'list': True, 'nullable': True, 'type': 'Author', args: {}}],
     ...
-    [{'field': 'books', 'list': True, 'nullable': True, 'type': 'Book'},
-     {'field': 'publisher', 'list': False, 'nullable': True, 'type': 'Publisher'}]
+    [{'field': 'books', 'list': True, 'nullable': True, 'type': 'Book', args: {}},
+     {'field': 'publisher', 'list': False, 'nullable': True, 'type': 'Publisher', args: {}}]
     ]
     """
     types = map_schema_types(schema, [])
 
     stack = []
     queries = _map_query_path(types['objects'], types['objects'][base], stack)
+    #map arguments to root fields
+    for query in queries:
+        # root field if it only has one field in the path
+        if len(query) == 1:
+            query[0]['args'] = _get_args(schema.query_type.fields, query[0]['field'])
+
     return queries
 
 def _map_query_path(types, base_type, stack):
@@ -29,13 +40,21 @@ def _map_query_path(types, base_type, stack):
         # if it's a field that leave us to another type then...
         if field['type'] in types.keys():
             #add this query path
-            queries.append([{'field':field_name, **field}])
+            queries.append([{'field':field_name, 'args':{}, **field}])
 
             # if the field type is not already in the stack, this is to avoid an infinite recursion
             if field['type'] not in stack:
                 stack.append(field['type'])
                 sub_queries = _map_query_path(types, types[field['type']], stack)
                 stack.pop()
-                queries += [[{'field':field_name, **field}] + sub_query for sub_query in sub_queries]
+                # prepend current query path to the returned queries
+                queries += [[{'field':field_name, 'args':{}, **field}] + sub_query for sub_query in sub_queries]
 
     return queries
+
+def _get_args(fields, field):
+    args = {}
+    if field in fields and len(fields[field].args):
+        args = [{arg_name: get_named_type(arg.type).name} for arg_name, arg in fields[field].args.items()]
+
+    return args
