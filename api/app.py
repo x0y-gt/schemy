6e88df55e9c12235d0dc759a8562325b0af90a73
x0y <x0y@milkyweb.co>
2019-06-30 20:05:53 +0200

    [add] Server support added to start the server running api/app.py

--- api/app.py
+++ api/app.py
@@ -1,18 +1,30 @@
+from aiohttp import web
+from aiohttp_graphql import GraphQLView
 from schemy import Schemy
 
 from api import config
 from api.model import datasource
 
-sdl_path = config.get('APP_ROOT_DIR') + config.get('APP_SDL_PATH')
-schemy = Schemy(sdl_path)
-schemy.types_path = config.get('APP_TYPES_DIR')
-schemy.datasource = datasource
-schemy.build()
 
+def bootstrap_schemy():
+    sdl_path = config.get('APP_ROOT_DIR') + config.get('APP_SDL_PATH')
+    schemy = Schemy(sdl_path)
+    schemy.types_path = config.get('APP_TYPES_DIR')
+    schemy.datasource = datasource
+    schemy.build()
+    return schemy
 
-query = """{
-    book(id: "1") {
-        name
-    }
-}"""
-print(schemy.handle(query))
+if __name__ == '__main__':
+    schemy = bootstrap_schemy()
+
+    app = web.Application()
+    # configure app
+    gql_view = GraphQLView(
+        schema=schemy.schema(),
+        field_resolver=schemy.get_resolver(),
+        graphiql=True
+    )
+    app.router.add_route('GET', '/graphql', gql_view, name='graphql')
+    app.router.add_route('POST', '/graphql', gql_view, name='graphql')
+
+    web.run_app(app, port=8080)
