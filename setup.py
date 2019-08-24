deeef64130e7029423ecf32d3a3db9c35f8c4582
x0y <x0y@milkyweb.co>
2019-08-24 12:00:19 -0600

    [fix] fixed dependencies link

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='schemy',
           'GraphQL-core-next>=1.0.3',
           'SQLAlchemy==1.3.4',
           'psycopg2-binary==2.8.2',
-          'aiohttp-graphql==1.0 @ git+https://github.com/x0y-gt/aiohttp-graphql@use-core-next#egg=aiohttp-graphql-1.0',
+          'aiohttp-graphql@https://github.com/x0y-gt/aiohttp-graphql@use-core-next#egg=aiohttp-graphql-1.0',
           'alembic==1.0.10',
           'factory-boy==2.12'
       ],
