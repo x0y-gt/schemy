9239bdcab15ebfdaa61c379842e16a30b7754461
x0y <x0y@milkyweb.co>
2019-09-01 12:11:35 -0600

    [add] Id postfix when filtering by root queries

--- schemy/renders/typemethod.py
+++ schemy/renders/typemethod.py
@@ -45,10 +45,11 @@ class TypeMethod(Base): #pylint: disable=too-many-instance-attributes,too-few-pu
 
         self._code.insert(0, '"""This method is to return elements filtered by')
         self._code.insert(1, 'the arguments defined in the root obj Query field')
-        self._code.insert(2, 'WARNING: Assuming the arguments exists as defined fields"""')
+        self._code.insert(2, 'WARNING: Assuming the arguments exists as foreign keys')
+        self._code.insert(3, 'in defined fields, adding "Id" to the arguments"""')
         self._code.append('query = self.query()')
 
-        filters = ['{name}={name}'.format(name=arg_name) for arg_name in self.args]
+        filters = ['{name}Id={name}'.format(name=arg_name) for arg_name in self.args]
 
         self._code.append('query.filter_by({filters})'.format(filters=', '.join(filters)))
         self._code.append('return query.all()')
